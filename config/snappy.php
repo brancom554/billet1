a07e9f0ad557b3c963ad5d19a78af9079153aae5
Marius DEV <brancom554@gmail.com>
2021-08-24 17:26:19 +0100

    snappy changes

--- config/snappy.php
+++ config/snappy.php
@@ -34,19 +34,20 @@ return [
     */
     
     'pdf' => [
-        'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
-        'timeout' => false,
-        'options' => [],
-        'env'     => [],
-    ],
+'enabled' => true,
+'binary'  => '"/usr/local/bin/wkhtmltopdf"',
+'timeout' => false,
+'options' => [],
+'env'     => [],
+],
     
-    'image' => [
-        'enabled' => true,
-        'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
-        'timeout' => false,
-        'options' => [],
-        'env'     => [],
-    ],
+'image' => [
+'enabled' => true,
+'binary'  => env('WKHTML_IMG_BINARY', '/usr/local/bin/wkhtmltoimage'),
+'timeout' => false,
+'options' => [],
+'env'     => [],
+],
+
 
 ];
